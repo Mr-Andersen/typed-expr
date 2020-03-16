4fdba30c0533de46ef7c17c0cd539640ad143a7d
Mr-Andersen <andrew-anderson.neo@yandex.ru>
2020-03-16 12:36:42 +0300

    Init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+struct My {
+    a: i32,
+    b: i32
+}
+
+
+fn procedure<'res, 'a: 'res, 'b: 'res>(
+    a: &'a My, b: &'b My) -> &'res My
+{
+    if a.a > b.b { a } else { b }
+}
+
+
+fn main() {
+    let a = My { a: 13, b: -1 };
+    let b = My { a: 10, b: 6 };
+    let x = procedure(&a, &b);
+    println!("{:?}", x);
+}
